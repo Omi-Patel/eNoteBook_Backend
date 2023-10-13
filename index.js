b7fad8b88e7bf7d08d49d1afa0321a58847c7889
Om Patel <omupatel7113@gmail.com>
2023-10-13 12:02:23 +0530

    Uploading Backend

---
+++ index.js
@@ -0,0 +1,23 @@
+import connectToMongo from "./database/db.js";
+import express from "express";
+import auth from "./routes/auth.js";
+import notes from "./routes/notes.js";
+import cors from "cors";
+
+connectToMongo();
+
+const app = express();
+const port = 4000;
+
+// middelwares
+app.use(express.json());
+app.use(cors());
+
+// Availabel Routes
+
+app.use("/api/auth", auth);
+app.use("/api/notes", notes);
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
